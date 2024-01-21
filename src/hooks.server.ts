76649159e1025a4a0d720beccae2578f71461459
SIMULATAN <simulatan2319@gmail.com>
2024-01-21 23:47:35 +0100

    Fix prod deployment, again
    Fuck you AuthJS

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,15 +4,13 @@ import {
     PRIVATE_OIDC_CLIENT_SECRET as OIDC_CLIENT_SECRET,
     PRIVATE_OIDC_SERVER_URL as OIDC_SERVER_URL,
 } from "$env/static/private";
-import {
-    PUBLIC_BASE_PATH as BASE_PATH
-} from "$env/static/public";
 import type { OIDCConfig } from "@auth/core/providers";
 import type { CasdoorProfile } from "./types";
 
 export const handle =
     SvelteKitAuth({
-        basePath: BASE_PATH,
+        // don't believe the "Default:", it's unset per default
+        basePath: "/auth",
         providers: [
             {
                 id: "oidc",
